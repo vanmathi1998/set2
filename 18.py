2f9168737f14c603cb6f01536641e327cd955ca6
vanmathi1998 <49603302+vanmathi1998@users.noreply.github.com>
2019-05-08 14:08:36 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,2 +1,11 @@
 lower=int(input("enter lower range:"))
 upper=int(input("enter upper range:"))
+for num in range(lower,upper+1):
+  sum=0
+  temp=num
+  while temp > 0:
+    digit = temp % 10
+    sum + = digit**3
+    temp // = 10
+    if num == sum:
+      print(num)
